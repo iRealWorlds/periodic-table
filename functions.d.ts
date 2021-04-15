a651205af130877d0b1612c879224078950420aa
iRealWorlds <calin.marin@codestage.ro>
2021-04-15 21:58:27 +0300

    Updates

--- functions.d.ts
+++
@@ -1,3 +0,0 @@
-import { Selection } from "d3";
-export declare const setStyle: (el: Selection<any, any, HTMLElement, any>, attrs: object) => Selection<any, any, HTMLElement, any>;
-export declare const setAttrs: (el: Selection<any, any, HTMLElement, any>, attrs: object) => Selection<any, any, HTMLElement, any>;
--- functions.js
+++
@@ -1,7 +0,0 @@
-export const setStyle = (el, attrs) => {
-    return Object.entries(attrs).reduce((acc, [key, val]) => acc.style(key, val), el);
-};
-export const setAttrs = (el, attrs) => {
-    return Object.entries(attrs).reduce((acc, [key, val]) => acc.attr(key, val), el);
-};
-//# sourceMappingURL=functions.js.map
